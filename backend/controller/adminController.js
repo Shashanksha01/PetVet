21d82197fa34b6f30dcc2bbaddecf0bb3ecdf12b
Shashanksha01 <123055297+Shashanksha01@users.noreply.github.com>
2025-04-30 10:57:18 +0530

    small changes

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -97,4 +97,19 @@ const addDoctor = async (req, res) => {
   }
 };
 
-export { addDoctor };
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+      
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: `Something went wrong ${error.message}`,
+    });
+  }
+};
+
+export { addDoctor, adminLogin };
